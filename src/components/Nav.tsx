40174995a7fac0c6aefdd8d526dd3db8bf777782
vlado <vladocavric@gmail.com>
2025-06-09 12:41:59 +0200

    finis

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,9 +4,27 @@ import { motion, AnimatePresence } from 'framer-motion';
 import '../styles/global.css';
 import { useMediaQuery } from '../util/useMediaQuery';
 
-const navMogion = {
+const navMotion = {
 	visible: {
 		opacity: 1,
+		transition: {
+			when: 'beforeChildren',
+			staggerChildren: 0.15,
+		},
+	},
+	hidden: {
+		opacity: 0,
+	},
+};
+
+const itemMotion = {
+	visible: {
+		opacity: 1,
+		x: 0,
+	},
+	hidden: {
+		opacity: 0,
+		x: -100,
 	},
 };
 
@@ -76,19 +94,24 @@ const Nav = () => {
 			)}
 			<AnimatePresence>
 				{toggle && !matches && (
-					<motion.div
-						key='box'
-						animate={{ opacity: 1, x: 0 }}
-						initial={{ opacity: 0, x: 25 }}
-						exit={{ opacity: 0, x: 25 }}
-						transition={{ duration: 0.3 }}
-						className='fixed bg-white top-0 left-0 w-full h-screen items-center justify-center flex'>
-						<div className='flex flex-col gap-24 text-lg'>
-							<a href='/'>Home</a>
-							<a href='/services'>Services</a>
-							<a href='/contact'>Contact</a>
-						</div>
-					</motion.div>
+					<div className='fixed bg-white top-0 left-0 w-full h-screen items-center justify-center flex'>
+						<motion.div
+							variants={navMotion}
+							animate='visible'
+							initial='hidden'
+							exit='hidden'
+							className='flex flex-col gap-24 text-lg'>
+							<motion.a variants={itemMotion} href='/'>
+								Home
+							</motion.a>
+							<motion.a variants={itemMotion} href='/services'>
+								Services
+							</motion.a>
+							<motion.a variants={itemMotion} href='/contact'>
+								Contact
+							</motion.a>
+						</motion.div>
+					</div>
 				)}
 			</AnimatePresence>
 		</nav>
